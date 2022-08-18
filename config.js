8cdcdee1f20bae2e7c68098ae93d58f92424a74d
zins-mdd <111218997+zins-mdd@users.noreply.github.com>
2022-08-18 16:42:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,24 +12,24 @@ global.APIKeys = {
 }
 
 // Other
-global.namabot = ['AzzBot-MD']
-global.namaowner = ['YahyaGanzz']
-global.owner = ['6281511480762']
-global.premium = ['6281511480762']
-global.packname = 'AzzBot-MD'
+global.namabot = ['ZINS BOTZ MD']
+global.namaowner = ['ZINS STORE REAL']
+global.owner = ['6282118415702']
+global.premium = ['6282118415702']
+global.packname = 'ZINS BOTZ MD'
 global.author = 'WhatsApp•Bot'
 global.sessionName = 'Xavior'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: 'DoaMu Terkabulkan',
+    success: 'Sukses✅',
     admin: 'Fitur Khusus Admin Group!',
     botAdmin: 'Jadikan Bot Admin Terlebih Dahulu!',
     owner: 'Khusus Owner Bot',
-    group: 'Itu Khusus Grup Bre...!',
-    private: 'Cuma Bisa Chat Pribadi Bre...',
+    group: 'Fitur Khusus Grup',
+    private: 'Fitur Khusus Chat Pribadi',
     bot: 'Khusus Nomor Bot',
-    wait: 'Tunggu Sejam...',
+    wait: 'Tunggu Sebentar',
     endLimit: 'Limit Harian Sudah Habis, Limit Akan Direset Besok',
 }
 //Kalo Ngasih Limit Jangan Dikit Lah Bng
